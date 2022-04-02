32ed5ca218a394902627a00da0f38a06a26f54f4
sssaleh94 <salehsalim94@gmail.com>
2022-04-02 12:14:44 +0300

    minor updates

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,312 +1,280 @@
 
 <!DOCTYPE html>
-
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>AdminLTE 3 | Starter</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Madrasat Munawwar</title>
 
-<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-
-<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-
-<link rel="stylesheet" href="dist/css/adminlte.min.css?v=3.2.0"> -->
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
-
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
 
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
 
-<ul class="navbar-nav">
-<li class="nav-item">
-<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-</li>
-<li class="nav-item d-none d-sm-inline-block">
-<a href="index3.html" class="nav-link">Home</a>
-</li>
-<li class="nav-item d-none d-sm-inline-block">
-<a href="#" class="nav-link">Contact</a>
-</li>
-</ul>
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+     
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li>
+      <!-- Notifications Dropdown Menu -->
 
-<ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
 
-<li class="nav-item">
-<a class="nav-link" data-widget="navbar-search" href="#" role="button">
-<i class="fas fa-search"></i>
-</a>
-<div class="navbar-search-block">
-<form class="form-inline">
-<div class="input-group input-group-sm">
-<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-<div class="input-group-append">
-<button class="btn btn-navbar" type="submit">
-<i class="fas fa-search"></i>
-</button>
-<button class="btn btn-navbar" type="button" data-widget="navbar-search">
-<i class="fas fa-times"></i>
-</button>
-</div>
-</div>
-</form>
-</div>
-</li>
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <!-- <img src="img/logo.png" alt="Logo" class="brand-image  mb-2 elevation-3" style="opacity: .8"> -->
+      <span class="brand-text font-weight-light">MMS</span>
+    </a>
 
-<li class="nav-item dropdown">
-<a class="nav-link" data-toggle="dropdown" href="#">
-<i class="far fa-comments"></i>
-<span class="badge badge-danger navbar-badge">3</span>
-</a>
-<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-<a href="#" class="dropdown-item">
+    <!-- Sidebar -->
+    <div class="sidebar">
 
-<div class="media">
-<img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-<div class="media-body">
-<h3 class="dropdown-item-title">
-Brad Diesel
-<span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-</h3>
-<p class="text-sm">Call me whenever you can...</p>
-<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-</div>
-</div>
-
-</a>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item">
-
-<div class="media">
-<img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-<div class="media-body">
-<h3 class="dropdown-item-title">
-John Pierce
-<span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-</h3>
-<p class="text-sm">I got your message bro</p>
-<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-</div>
-</div>
-
-</a>
-<div class="dropdown-divider"></div>
- <a href="#" class="dropdown-item">
-
-<div class="media">
-<img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-<div class="media-body">
-<h3 class="dropdown-item-title">
-Nora Silvester
-<span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-</h3>
-<p class="text-sm">The subject goes here</p>
-<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-</div>
-</div>
-
-</a>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-</div>
-</li>
-
-<li class="nav-item dropdown">
-<a class="nav-link" data-toggle="dropdown" href="#">
-<i class="far fa-bell"></i>
-<span class="badge badge-warning navbar-badge">15</span>
-</a>
-<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-<span class="dropdown-header">15 Notifications</span>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item">
-<i class="fas fa-envelope mr-2"></i> 4 new messages
-<span class="float-right text-muted text-sm">3 mins</span>
-</a>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item">
-<i class="fas fa-users mr-2"></i> 8 friend requests
-<span class="float-right text-muted text-sm">12 hours</span>
-</a>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item">
-<i class="fas fa-file mr-2"></i> 3 new reports
-<span class="float-right text-muted text-sm">2 days</span>
-</a>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-</div>
-</li>
-<li class="nav-item">
-<a class="nav-link" data-widget="fullscreen" href="#" role="button">
-<i class="fas fa-expand-arrows-alt"></i>
-</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-<i class="fas fa-th-large"></i>
-</a>
-</li>
-</ul>
-</nav>
-
-
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-
-<a href="index3.html" class="brand-link">
-<img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-
-</a>
-
-<div class="sidebar">
-
-<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          @include('layouts.menu')
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
     
+    <!-- /.sidebar -->
+  </aside>
 
-<nav class="mt-2">
-<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Starter Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-<li class="nav-item menu-open">
-<a href="#" class="nav-link active">
-<i class="nav-icon fas fa-tachometer-alt"></i>
-<p>
-Starter Pages
-<i class="right fas fa-angle-left"></i>
-</p>
-</a>
-<ul class="nav nav-treeview">
-<li class="nav-item">
-<a href="#" class="nav-link active">
-<i class="far fa-circle nav-icon"></i>
-<p>Active Page</p>
-</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-<i class="far fa-circle nav-icon"></i>
-<p>Inactive Page</p>
-</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-<i class="nav-icon fas fa-th"></i>
-<p>
-Simple Link
-<span class="right badge badge-danger">New</span>
-</p>
-</a>
-</li>
-</ul>
-</nav>
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>150</h3>
 
-</div>
+                <p>New Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
 
-</aside>
+          <!-- another badge  -->
 
-<div class="content-wrapper">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-<div class="content-header">
-<div class="container-fluid">
-<div class="row mb-2">
-<div class="col-sm-6">
-<h1 class="m-0">Starter Page</h1>
-</div>
-<div class="col-sm-6">
-<ol class="breadcrumb float-sm-right">
-<li class="breadcrumb-item"><a href="#">Home</a></li>
-<li class="breadcrumb-item active">Starter Page</li>
-</ol>
-</div>
-</div>
-</div>
-</div>
+                <p>Bounce Rate</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
 
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
 
-<div class="content">
-<div class="container-fluid">
-<div class="row">
-<div class="col-lg-6">
-<div class="card">
-<div class="card-body">
-<h5 class="card-title">Card title</h5>
-<p class="card-text">
-Some quick example text to build on the card title and make up the bulk of the card's
-content.
-</p>
-<a href="#" class="card-link">Card link</a>
-<a href="#" class="card-link">Another link</a>
-</div>
-</div>
-<div class="card card-primary card-outline">
-<div class="card-body">
-<h5 class="card-title">Card title</h5>
-<p class="card-text">
-Some quick example text to build on the card title and make up the bulk of the card's
-content.
-</p>
-<a href="#" class="card-link">Card link</a>
-<a href="#" class="card-link">Another link</a>
-</div>
-</div>
-</div>
+                <p>User Registrations</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
 
-<div class="col-lg-6">
-<div class="card">
-<div class="card-header">
-<h5 class="m-0">Featured</h5>
-</div>
-<div class="card-body">
-<h6 class="card-title">Special title treatment</h6>
-<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-<a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
-<div class="card card-primary card-outline">
-<div class="card-header">
-<h5 class="m-0">Featured</h5>
-</div>
-<div class="card-body">
-<h6 class="card-title">Special title treatment</h6>
-<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-<a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
-</div>
- 
-</div>
+                <p>Unique Visitors</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- /.col-md-6 -->
 
-</div>
-</div>
-
-</div>
+          
 
 
-<aside class="control-sidebar control-sidebar-dark">
+          <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-7 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="fas fa-chart-pie mr-1"></i>
+                  Sales
+                </h3>
+                <div class="card-tools">
+                  <ul class="nav nav-pills ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                    </li>
+                  </ul>
+                </div>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content p-0">
+                  <!-- Morris chart - Sales -->
+                  <div class="chart tab-pane active" id="revenue-chart"
+                       style="position: relative; height: 300px;">
+                      <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
+                   </div>
+                  <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+                    <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
+                  </div>
+                </div>
+              </div><!-- /.card-body -->
+            </div>
+          </section>
+          </div>
 
-<div class="p-3">
-<h5>Title</h5>
-<p>Sidebar content</p>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
 </div>
-</aside>
+<!-- ./wrapper -->
 
-
-<footer class="main-footer">
-
-<div class="float-right d-none d-sm-inline">
-Anything you want
-</div>
-
-<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-</footer>
-</div>
-
-
-
+<!-- REQUIRED SCRIPTS -->
 
 <script src="{{asset('js/app.js')}}"></script>
+<script src="plugins/chart.js/Chart.min.js"></script>
 </body>
 </html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,65 @@
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+            Dashboard
+            </p>
+    </a>
+    
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+            <p>
+            Users
+            </p>
+    </a>
+    
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user-graduate"></i>
+            <p>
+            Students
+            </p>
+    </a>
+    
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user-tie"></i>
+            <p>
+            Parents
+            </p>
+    </a>
+    
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+            Classes
+            </p>
+    </a>
+    
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-bullhorn"></i>
+            <p>
+            Announcement
+            </p>
+    </a>
+    
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-file-lines"></i>
+    <p>
+    Report
+    
+    </p>
+    </a>
+</li>
